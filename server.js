e2edbd2f9b560a9b476295f0e96ccd733fd57368
Ding Liu <movingon.ding@gmail.com>
2018-02-04 14:37:24 -0700

    user can now edit and delete saved URLs

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express')
 const qs = require('qs')
 const app = express()
 const PORT = 3000
+const bodyParser = require("body-parser")
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -30,6 +32,46 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", { urls: urlDatabase });
 });
 
-app.get("/urls/:id", (req, res) => {
-  res.render("urls_show", { shortURL: req.params.id });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
+
+app.post("/urls", (req, res) => {
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  // res.send("Ok: "+ shortURL);
+  res.redirect("/u/" + shortURL);   
+});
+
+app.post("/urls/delete/:id", (req, res) => {
+  let shortURL = req.params.id;
+  delete urlDatabase[shortURL];
+  res.render("urls_index", { urls: urlDatabase });   
+});
+
+app.get("/urls/:id", (req, res) => {
+  res.render("urls_show", { shortURL: req.params.id, longURL: urlDatabase[req.params.id] });   
+});
+
+app.post("/urls/:id", (req, res) => {
+  let shortURL = req.params.id;
+  urlDatabase[shortURL] = req.body.longURL;
+  res.render("urls_index", { urls: urlDatabase });   
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+function generateRandomString() {
+  var text = "";
+  var size = 6;
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < size; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
